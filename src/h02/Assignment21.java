9332994aa7f1fa62ad94f19be432585d0be2ef1d
YousefAbood <Yousefhabood@gmail.com>
2020-09-06 13:19:59 +0200

    Assignment21, Name in Yellow, Blue Background

---
+++ src/h02/Assignment21.java
@@ -0,0 +1,16 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Assignment21 extends Applet {
+
+    public void init () {
+        setBackground(Color.blue);
+    }
+
+    public void paint (Graphics g) {
+        g.setColor (Color.yellow);
+        g.drawString("Yousef AlGeefary", 50, 60);
+    }
+}

c2bd354684aeaef68dc1dd575c2e9bd63dbddc99
YousefAbood <Yousefhabood@gmail.com>
2020-09-06 13:45:16 +0200

    Assignment22, Backgroundcolor White Roepnaam Blue Achternaam Red

--- src/h02/Assignment22.java
+++ src/h02/Assignment22.java
@@ -1,4 +1,20 @@
 package h02;
 
-public class Assignment22 {
+import java.applet.Applet;
+import java.awt.*;
+
+public class Assignment22 extends Applet {
+
+    public void init () {
+        setBackground(Color.white);
+    }
+
+    public void paint (Graphics g) {
+        g.setColor (Color.blue);
+        g.setFont (new Font ("TimesRoman", Font.BOLD | Font.ITALIC, 20));
+        g.drawString("Yousef", 50, 60);
+        g.setColor(Color.red);
+        g.drawString("Abood", 50, 100);
+    }
+
 }

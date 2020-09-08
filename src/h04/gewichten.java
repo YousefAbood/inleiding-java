9e982ec347d871395cf5fe869350b07826950e36
YousefAbood <Yousefhabood@gmail.com>
2020-09-08 16:47:42 +0200

    Klaar

--- src/h04/gewichten.java
+++ src/h04/gewichten.java
@@ -3,42 +3,46 @@ package h04;
 import java.applet.Applet;
 import java.awt.*;
 
-public class nederlandsevlag extends Applet {
+public class gewichten extends Applet {
 
-    public void init() {
+    public void init(){
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int) screenSize.getWidth();
         int height = (int) screenSize.getHeight();
         resize(width, height);
     }
 
-    public void paint(Graphics g) {
+    public void paint(Graphics g){
 
         // Sky Color
         Color SkyColor = new Color(69, 182, 217);
         setBackground(SkyColor);
 
-        // Sun
-        g.setColor(Color.yellow);
-        g.fillOval(1100, 50, 200, 200);
+        // Bar x, y axis
+        Color DarkMagenta = new Color(177, 2, 166);
+        g.setColor(DarkMagenta);
+        g.setFont (new Font ("TimesRoman", Font.BOLD | Font.ITALIC, 20));
+        g.drawString("Gewichten", 80, 60);
+        g.drawString("Names", 1175, 590);
 
-        // Rood Stuk
+        // X Axis
+        g.setColor(Color.black);
+        g.fillRect(150, 580, 1000, 15);
+
+        // Y Axis
+        g.fillRect(150, 80, 15, 500);
+
+        // Valerie 40kg
         Color Red = new Color(174, 28, 40);
         g.setColor(Red);
-        g.fillRect(150, 190, 600, 150);
 
-        // Wit Stuk
+        // Jeroen 100kg
         Color White = new Color(255, 255, 255);
         g.setColor(White);
-        g.fillRect(150, 290, 600, 150);
 
-        // Blue Stuk
+        // Hans 80kg
         Color Blue = new Color(33, 70, 139);
         g.setColor(Blue);
-        g.fillRect(150, 390, 600, 100);
-        g.setColor(Color.black);
-        g.fillRect(150, 190, 15, 500);
-        // VlaggenStok
 
     }
 }

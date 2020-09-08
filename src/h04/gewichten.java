3dbccd8aa4c0e307b41e9dc11a17bd2e7dfcf1ae
YousefAbood <Yousefhabood@gmail.com>
2020-09-08 18:47:25 +0200

    Klaar

--- src/h04/gewichten.java
+++ src/h04/gewichten.java
@@ -24,25 +24,34 @@ public class gewichten extends Applet {
         g.setFont (new Font ("TimesRoman", Font.BOLD | Font.ITALIC, 20));
         g.drawString("Gewichten", 80, 60);
         g.drawString("Names", 1175, 590);
+        g.drawString("Valerie", 350, 625);
+        g.drawString("Jeroen", 650, 625);
+        g.drawString("Hans", 950, 625);
 
-        // X Axis
+        // Y Axis, NOTE, 85 PERFECT VALUE FOR HEIGHT 500!!!!!!!!!!!!!
+
+        // Equation for Variables, For every number you subtract from height(500), you add to Y Axis (85)!
+        // Vice Versa, Add to height, you subtract from Y Axis
         g.setColor(Color.black);
-        g.fillRect(150, 580, 1000, 15);
-
-        // Y Axis
         g.fillRect(150, 80, 15, 500);
 
         // Valerie 40kg
-        Color Red = new Color(174, 28, 40);
-        g.setColor(Red);
+        Color Blue = new Color(33, 70, 139);
+        g.setColor(Blue);
+        g.fillRect(350, 425, 75, 160);
 
         // Jeroen 100kg
         Color White = new Color(255, 255, 255);
         g.setColor(White);
+        g.fillRect(650, 185, 75, 400);
 
         // Hans 80kg
-        Color Blue = new Color(33, 70, 139);
-        g.setColor(Blue);
+        Color Red = new Color(174, 28, 40);
+        g.setColor(Red);
+        g.fillRect(950, 265, 75, 320);
 
+        // X Axis
+        g.setColor(Color.black);
+        g.fillRect(150, 580, 1000, 15);
     }
 }

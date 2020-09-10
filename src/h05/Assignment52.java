f9f27878bcb615a9ce845f3df156e95e85b03609
YousefAbood <Yousefhabood@gmail.com>
2020-09-10 10:23:12 +0200

    Klaar

---
+++ src/h05/Assignment52.java
@@ -0,0 +1,62 @@
+package h05;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Assignment52 extends Applet{
+
+
+        public void init(){
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            int width = (int) screenSize.getWidth();
+            int height = (int) screenSize.getHeight();
+            resize(width, height);
+        }
+
+        public void paint(Graphics g){
+
+            // Sky Color
+            Color SkyColor = new Color(69, 182, 217);
+            setBackground(SkyColor);
+
+            // Bar x, y axis
+            Color DarkMagenta = new Color(177, 2, 166);
+            g.setColor(DarkMagenta);
+            g.setFont (new Font ("TimesRoman", Font.BOLD | Font.ITALIC, 20));
+            g.drawString("Gewichten", 80, 60);
+            g.drawString("Names", 1175, 590);
+            g.drawString("Valerie", 350, 625);
+            g.drawString("Jeroen", 650, 625);
+            g.drawString("Hans", 950, 625);
+
+            // Y Axis, NOTE, 85 PERFECT VALUE FOR HEIGHT 500!!!!!!!!!!!!!
+
+
+            g.setColor(Color.black);
+            g.fillRect(150, 80, 15, 500);
+
+            // Valerie 40kg
+            int valerie = 160;
+            Color Blue = new Color(33, 70, 139);
+            g.setColor(Blue);
+            g.fillRect(350, 85+(500-valerie), 75, valerie);
+
+            // Jeroen 100kg
+            int jeroen = 400;
+            Color White = new Color(255, 255, 255);
+            g.setColor(White);
+            g.fillRect(650, 85+(500-jeroen), 75, jeroen);
+
+            // Hans 80kg
+            int hans = 320;
+            Color Red = new Color(174, 28, 40);
+            g.setColor(Red);
+            g.fillRect(950, 85+(500-hans), 75, hans);
+
+            // X Axis
+            g.setColor(Color.black);
+            g.fillRect(150, 580, 1000, 15);
+        }
+    }
+
+

31d0144e09d39c1c873f059a2b83e3ce6a02d80b
YousefAbood <Yousefhabood@gmail.com>
2020-09-20 21:11:41 +0200

    Created submitClass(Class) & Instance (submitEvent) & Button (submitButton) & Variables

---
+++ src/h11/Assignment120.java
@@ -0,0 +1,46 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Assignment120 extends Applet {
+
+    Button submitButton;
+    Label labelTextField;
+    TextField textField;
+    String getText;
+    int i, input, output, getTextstrToInt;
+    Color SkyColor = new Color(69, 182, 217);
+
+    public void init() {
+
+        // Background Color
+        setBackground(SkyColor);
+
+        // Screen Size
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+        resize(width, height);
+
+        // Button
+        submitButton = new Button(">> Submit <<");
+        submitClass submitEvent = new submitClass();
+        submitButton.addActionListener(submitEvent);
+
+
+    }
+
+    public void paint(Graphics g) {
+        for (i = 0; i < 12; i++) {
+
+        }
+    }
+
+   class submitClass implements ActionListener {
+       public void actionPerformed(ActionEvent e) {
+
+       }
+   }
+}

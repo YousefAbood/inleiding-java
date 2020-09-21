acf2b551d68680f1f1ecae5bd0bcef0a4ae51e53
YousefAbood <Yousefhabood@gmail.com>
2020-09-21 15:40:47 +0200

    Created Generation Class, Instance (generateEvent) for Button, For-Loop, Initialized x, y, i, random.

--- src/h12/Assignment121.java
+++ src/h12/Assignment121.java
@@ -2,9 +2,14 @@ package h12;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
 
 public class Assignment121 extends Applet {
 
+    Button generateButton;
+    int x, y, i, output, average, random;
     int[] numbers;
 
     public void init() {
@@ -19,15 +24,34 @@ public class Assignment121 extends Applet {
         int height = screensize.height;
         resize(width, height);
 
+        // Button
+        generateButton = new Button(">> Generate <<");
+        generateClass generateEvent = new generateClass();
+        generateButton.addActionListener(generateEvent);
+
         // Variable
-        numbers[0] = 24;
+        x = 20;
+        y = 20;
 
-
+        // Add
+        add(generateButton);
     }
 
     public void paint(Graphics g) {
-
+        g.setColor(Color.black);
+        g.setFont(new Font("Courier", Font.BOLD, 20));
+        g.drawString("Index " + i + " >> " + random, x, y);
     }
 
-
+    class generateClass implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            for (i = 0; i < 11; i++) {
+                x += 50;
+                y += 50;
+                random = (int)(Math.random() * 50 +1);
+                numbers[i] = random;
+                repaint();
+            }
+        }
+    }
 }

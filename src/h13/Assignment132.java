33e959071ce57c75c6be715b2c55f1b1a0cde321
YousefAbood <Yousefhabood@gmail.com>
2020-09-26 18:41:54 +0200

    Created Method, Now it draws first row of Dark Red Bricks.

---
+++ src/h13/Assignment132.java
@@ -0,0 +1,34 @@
+package h13;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Assignment132 extends Applet {
+
+    int i, v, x, y, w, h;
+    Color brickRed = new Color(139, 0, 0);
+
+    public void init() {
+        // Variables
+        x = -150;
+        y = 0;
+        w = 150;
+        h = 100;
+    }
+
+    public void paint(Graphics g) {
+        drawRedBricks(g);
+    }
+
+    void drawRedBricks(Graphics g) {
+        for (i = 0; i < 200; i++) {
+
+            x += 150;
+            g.setColor(brickRed);
+            g.fillRect(x, y, w, h);
+            g.setColor(Color.black);
+            g.drawRect(x, y, w, h);
+
+        }
+    }
+}

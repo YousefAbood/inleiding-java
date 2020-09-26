4aad91ff69ed346429e7484da99fc3615264bda4
YousefAbood <Yousefhabood@gmail.com>
2020-09-26 19:39:48 +0200

    Method now covers the entire screen with Dark Red Bricks

--- src/h13/Assignment132.java
+++ src/h13/Assignment132.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class Assignment132 extends Applet {
 
-    int i, v, x, y, w, h;
+    int i, v, x, y, w, h, rows;
     Color brickRed = new Color(139, 0, 0);
 
     public void init() {
@@ -22,13 +22,16 @@ public class Assignment132 extends Applet {
 
     void drawRedBricks(Graphics g) {
         for (i = 0; i < 200; i++) {
-
+            rows++;
             x += 150;
             g.setColor(brickRed);
             g.fillRect(x, y, w, h);
             g.setColor(Color.black);
             g.drawRect(x, y, w, h);
-
+            if (rows%9 == 0) {
+                x = -150;
+                y += 100;
+            }
         }
     }
 }

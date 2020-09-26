ecb2da1d533652f89648d98b00556e006478ea0a
YousefAbood <Yousefhabood@gmail.com>
2020-09-26 21:14:00 +0200

    New Method, Erase. Switches between Red & Grey on request after Erase has been clicked.

--- src/h13/Assignment133.java
+++ src/h13/Assignment133.java
@@ -2,14 +2,36 @@ package h13;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Assignment133 extends Applet {
 
-    int i, v, x, y, w, h, cw, ch, rows;
+    Boolean activeRed = Boolean.FALSE;
+    Boolean activeGrey = Boolean.FALSE;
+    Button redBricks, greyBricks, eraseButton;
+    int i, v, x, y, w, h, cw, ch, rowsRed, rowsGrey;
     Color brickRed = new Color(139, 0, 0);
     Color concreteGray = new Color(102, 102, 102);
 
     public void init() {
+
+        // Buttons
+
+        // Red Button
+        redBricks = new Button(">> Click Here To Draw Red Bricks <<");
+        redBricksClass redBricksEvent = new redBricksClass();
+        redBricks.addActionListener(redBricksEvent);
+
+        // Grey Button
+        greyBricks = new Button(">> Click Here To Draw Grey Bricks <<");
+        greyBricksClass greyBricksEvent = new greyBricksClass();
+        greyBricks.addActionListener(greyBricksEvent);
+
+        // Erase Button
+        eraseButton = new Button(">> Click here to Erase <<");
+        eraseClass eraseEvent = new eraseClass();
+        eraseButton.addActionListener(eraseEvent);
+
         // Variables
         x = -150;
         y = 0;
@@ -17,21 +39,37 @@ public class Assignment133 extends Applet {
         h = 100;
         cw = 200;
         ch = 150;
+
+        // Add
+        add(redBricks);
+        add(greyBricks);
+        add(eraseButton);
     }
 
     public void paint(Graphics g) {
-        drawGreyBricks(g);
+        System.out.println("ActiveRed >> " + rowsRed);
+        System.out.println("ActiveGrey >> " + rowsGrey);
+
+        if(activeRed) {
+            System.out.println("Received");
+            drawRedBricks(g);
+        }
+
+        if(activeGrey) {
+            System.out.println("Received");
+            drawGreyBricks(g);
+        }
     }
 
     void drawRedBricks(Graphics g) {
         for (i = 0; i < 200; i++) {
-            rows++;
+            rowsRed++;
             x += 150;
             g.setColor(brickRed);
             g.fillRect(x, y, w, h);
             g.setColor(Color.black);
             g.drawRect(x, y, w, h);
-            if (rows%9 == 0) {
+            if (rowsRed%9 == 0) {
                 x = -150;
                 y += 100;
             }
@@ -40,16 +78,42 @@ public class Assignment133 extends Applet {
 
     void drawGreyBricks(Graphics g) {
         for (v = 0; v < 200; v++) {
-            rows++;
+            rowsGrey++;
             x += 150;
             g.setColor(concreteGray);
             g.fillRect(x, y, cw, ch);
             g.setColor(Color.black);
             g.drawRect(x, y, cw, ch);
-            if (rows%9 == 0) {
+            if (rowsGrey%9 == 0) {
                 x = -150;
                 y += 100;
             }
+        }
+    }
+
+    class redBricksClass implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            activeRed = Boolean.TRUE;
+            repaint();
+        }
+    }
+
+    class greyBricksClass implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            activeGrey = Boolean.TRUE;
+            repaint();
+        }
+    }
+
+    class eraseClass implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            activeGrey = Boolean.FALSE;
+            activeRed = Boolean.FALSE;
+            x = -150;
+            y = 0;
+            rowsGrey = 0;
+            rowsRed = 0;
+            repaint();
         }
     }
 }

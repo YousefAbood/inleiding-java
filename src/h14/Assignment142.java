fdfc8dbb3d593c103c848539d361ffd7a61f0345
YousefAbood <Yousefhabood@gmail.com>
2020-09-29 15:29:21 +0200

    Fixed Bug, Added Shuffling Sound

--- src/h14/Assignment142.java
+++ src/h14/Assignment142.java
@@ -1,22 +1,27 @@
 package h14;
 
 import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.*;
 import java.awt.event.*;
+import java.net.URL;
 import java.util.*;
 // Idea, Make it Numerical then Shuffle it.
 
 public class Assignment142 extends Applet {
+    URL path;
+    AudioClip sound;
+
     Boolean active = Boolean.FALSE;
     Button serveButton;
     String[] Color = {"Clubs", "Diamonds", "Hearts", "Spades"};
     String[] Type = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
     String[] Deck = new String[52];
+    String[] newLength = new String[52];
     String Input;
     int w, v, z, a, b, c, d, y, randomNumber, shuffledDeckIndex;
     int l = 0;
     int t = -1;
-    int i = -1;
 
     public void init() {
 
@@ -29,6 +34,10 @@ public class Assignment142 extends Applet {
         serveClass serveEvent = new serveClass();
         serveButton.addActionListener(serveEvent);
 
+        // Sound
+        path = Assignment142.class.getResource("/resources/");
+        sound = getAudioClip(path, "217501__vkproduktion__shuffling-cards-02.wav");
+
         // Add
         add(serveButton);
     }
@@ -36,6 +45,8 @@ public class Assignment142 extends Applet {
     public void paint(Graphics g) {
         g.setColor(java.awt.Color.black);
         g.setFont(new Font("Courier", Font.BOLD, 15));
+
+
         if (active) {
 
             // Player Number 1
@@ -69,6 +80,7 @@ public class Assignment142 extends Applet {
     }
 
     void serveMethod(Graphics g, int x, int y) {
+
         // We shuffle the cards
         // Make an array with a length less than the original Deck
         // Random Number Generation
@@ -94,11 +106,13 @@ public class Assignment142 extends Applet {
 
 
     void cardGeneration() {
+        Deck = newLength;
 
         // For Loop as long as the array
         // T adds the type
         // L adds the Color
         // When we finish from the first color we go to the second one
+        System.out.println("Deck >> " + Deck.length);
         for (v = 0; v < Deck.length; v++) {
             t++;
             if (v%13 == 0 && !(v == 0)) {
@@ -114,12 +128,16 @@ public class Assignment142 extends Applet {
         for (w = 0; w < 52; w++) {
             System.out.println("Deck W >> " + Deck[w]);
         }
+
     }
 
     class serveClass implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            l = 0;
+            t = -1;
             cardGeneration();
             active = Boolean.TRUE;
+            sound.play();
             repaint();
         }
     }

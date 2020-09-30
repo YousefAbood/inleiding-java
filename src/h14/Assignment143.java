6bb080ca5e92898994dba98a0c8e8d38ee493da1
YousefAbood <Yousefhabood@gmail.com>
2020-09-30 16:16:37 +0200

    Shows 24 Chess Pieces, Submit works, Reset Works, Invalid Number.

--- src/h14/Assignment143.java
+++ src/h14/Assignment143.java
@@ -8,12 +8,24 @@ import java.net.*;
 
 public class Assignment143 extends Applet {
 
-    // Buttons
-    Button beginButton, resetButton, submitButton;
+    // For-Loop
+    int i;
 
-    // TextField & Label
+    // Boolean
+    Boolean active = Boolean.FALSE;
+    // Not Correct Value Boolean
+    Boolean outOfBounds = Boolean.FALSE;
+
+    // Coordinates & Dimensions
+    int x, y, w, h;
+
+    // Buttons
+    Button resetButton, submitButton;
+
+    // TextField & Label & Parse
     Label textFieldLabel;
     TextField playerInputTextfield;
+    String getText;
 
     // Integers for the Player Input & Computer Input
     int playerNumber, computerNumber, imageNumber;
@@ -39,38 +51,85 @@ public class Assignment143 extends Applet {
         submitClass submitEvent = new submitClass();
         submitButton.addActionListener(submitEvent);
 
-        // Begin & Reset Buttons
-        beginButton = new Button(">> Begin <<");
+        // Reset Button
         resetButton = new Button(">> Reset <<");
-        beginResetClass beginResetEvent = new beginResetClass();
-        beginButton.addActionListener(beginResetEvent);
+        resetClass beginResetEvent = new resetClass();
         resetButton.addActionListener(beginResetEvent);
 
         // TextField
-        textFieldLabel = new Label("Put in a value, 1 2 3 then Click on Submit, To play again press Reset, To start playing Click on Begin. >> ");
+        textFieldLabel = new Label("Put in a value, 1 2 3 then Click on Submit, To play again press Reset. >> ");
         playerInputTextfield = new TextField("", 20);
+        playerInputTextfield.addActionListener(submitEvent);
+
+        // Variables
+        imageNumber = 24;
+        x = 150;
+        y = 50;
+        w = 80;
+        h = 80;
 
         // Add
         add(textFieldLabel);
-        add(beginButton);
         add(playerInputTextfield);
         add(submitButton);
         add(resetButton);
     }
 
     public void paint(Graphics g) {
-        g.drawImage(chessPiece, 300, 100, 80, 80, this);
+        g.setFont(new Font("Courier", Font.BOLD, 20));
+        if (outOfBounds) {
+            g.setColor(Color.red);
+            g.drawString("You put an Invalid Number, Try Again!", 50, 50);
+        }
+        active = Boolean.TRUE;
+        drawingObjects(g);
+        g.drawString("Value You Submitted >> " + playerNumber, 75, 75);
+        g.drawString("Value the Computer Submitted >> " + computerNumber, 100, 100);
+    }
+
+    void drawingObjects(Graphics g) {
+        x = 150;
+        y = 50;
+        if (active) {
+            g.setColor(Color.black);
+            for (i = 0; i <= imageNumber; i++) {
+                x += 100;
+                if (i % 5 == 0) {
+                    x = 150;
+                    y += 100;
+                }
+                g.drawImage(chessPiece, x, y, w, h, this);
+            }
+
+        }
     }
 
     class submitClass implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            x = 150;
+            y = 50;
+            getText = playerInputTextfield.getText();
+            playerNumber = Integer.parseInt(getText);
+            if(playerNumber <= 0 || playerNumber > 3) {
+                outOfBounds = Boolean.TRUE;
+            }
 
-        }
-    }
-
-    class beginResetClass implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
+            else {
+                outOfBounds = Boolean.FALSE;
+                imageNumber -= playerNumber;
+            }
             repaint();
         }
     }
+
+    class resetClass implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            imageNumber = 24;
+            x = 150;
+            y = 50;
+            repaint();
+        }
+    }
+
+
 }
